a4f0d46fbf294477e12fc5b7bd4e6261a14a772e
Sabarno Chatterjee <106575210+Sabarno-Chatterjee@users.noreply.github.com>
2022-10-02 20:36:06 +0530

    Update various_practice.py

--- various_practice.py
+++ various_practice.py
@@ -96,7 +96,7 @@ def days_in_month(year, month):
         return 29
     return month_days[month - 1]
 
-year = int(input("Enter a year: "))
-month = int(input("Enter a month: "))
+year = int(input("Enter a year: \n"))
+month = int(input("Enter a month: \n"))
 days = days_in_month(year, month)
 print(days)
